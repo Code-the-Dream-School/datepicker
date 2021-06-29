76cc2a0dc8e8523dca7c566b43d17c18476f183c
jrmcgarvey <jmcgarve@bellsouth.net>
2021-06-28 23:03:37 -0400

    first commit

---
+++ app/views/my_dates/_my_date.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! my_date, :id, :dt, :created_at, :updated_at
+json.url my_date_url(my_date, format: :json)
---
+++ db/migrate/20210629005336_create_my_dates.rb
@@ -0,0 +1,9 @@
+class CreateMyDates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :my_dates do |t|
+      t.datetime :dt
+
+      t.timestamps
+    end
+  end
+end
